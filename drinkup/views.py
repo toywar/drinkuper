f5c24168676ec90a081131e2f2fa0c990a2a23df
romek <admin@toywar.ru>
2013-07-31 01:13:45 +0400

    add repositories on GitHub

---
+++ drinkup/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+from django.template import loader, Context
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def base(request):
+  posts = Post.objects.all()
+  t = loader.get_template("base.html")
+  c = Context ({ 'posts': posts })
+  return HttpResponse(t.render(c))
+
+def view_base(request):
+  return render(request, 'base.html')
+  
