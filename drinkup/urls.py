4ae71e9e78401df265f9ecb31089cea3d301eb4b
romek <admin@toywar.ru>
2013-09-17 14:43:02 +0400

    modify urls: add /admin/ page for administrative django-site

--- drinkup/urls.py
+++ drinkup/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import *
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^$', 'drinkup.views.view_base'),
     url(r'^about/$', 'drinkup.views.view_about'),
     url(r'^past/$', 'drinkup.views.view_past'),
+    url(r'^admin/', include(admin.site.urls)),
 )
